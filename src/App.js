31fb46ef10e053ca3c65c784847637aa53b4b998
thomas jubin <thomas.jubin92@gmail.com>
2023-05-22 17:59:17 +0200

    add list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import Form from "./components/Form/Form";
+import List from "./components/List/List";
+import useLocalstorageState from "use-local-storage-state";
 import { uid } from "uid";
 
-import { useState } from "react";
 const initial = [
   { id: "99", name: "walking", isGoodWeatherActivity: true },
   { id: "100", name: "ski", isGoodWeatherActivity: false },
 ];
 const App = () => {
-  const [activities, setActivities] = useState(initial);
+  const [activities, setActivities] = useLocalstorageState("activities", {
+    defaultValue: initial,
+  });
 
   const handleAddActivity = (newActivity, isWeather) => {
     setActivities([{ name: newActivity, id: uid(), isWeather }, ...activities]);
@@ -16,7 +19,12 @@ const App = () => {
 
   console.log(activities);
 
-  return <Form onAddActivity={handleAddActivity} />;
+  return (
+    <>
+      <List activities={activities} />
+      <Form onAddActivity={handleAddActivity} />
+    </>
+  );
 };
 
 export default App;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,5 +1,15 @@
 import classes from "./List.module.css";
 
-const List = () => {};
+const List = ({ activities }) => {
+  return (
+    <ul className={classes.liContainer}>
+      {activities.map((element) => (
+        <li key={element.id} className={classes.listItem}>
+          {element.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default List;
